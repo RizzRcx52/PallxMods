93bbde3b333bb623dcd993f4a8685f19ea173f95
RizzRcx52 <160110402+RizzRcx52@users.noreply.github.com>
2024-03-11 22:25:05 +0700

    Add files via upload

---
+++ resources/install.sh
@@ -0,0 +1,21 @@
+sudo apt-get install golang-go
+sudo apt-get update && sudo apt-get upgrade
+pip install golang-go
+npm i requests
+npm i https-proxy-agent
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i cloudflare-bypasser
+npm audit fix
+pip3 install -r requirements.txt
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install ./google-chrome-stable_current_amd64.deb
+ulimit -n 999999
+chmod 777 *
